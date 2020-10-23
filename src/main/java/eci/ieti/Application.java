74bfae711ff2b6003ee4dd0c54b1bde1432ccaf7
julianagarzond <juliana.garzon-d@mail.escuelaing.edu.co>
2020-10-22 21:05:43 -0500

    part2

--- src/main/java/eci/ieti/Application.java
+++ src/main/java/eci/ieti/Application.java
@@ -4,6 +4,7 @@ import eci.ieti.data.CustomerRepository;
 import eci.ieti.data.ProductRepository;
 import eci.ieti.data.TodoRepository;
 import eci.ieti.data.UserRepository;
+import eci.ieti.data.config.AppConfiguration;
 import eci.ieti.data.model.Customer;
 import eci.ieti.data.model.Product;
 
@@ -13,9 +14,16 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.data.domain.PageRequest;
+import org.springframework.data.mongodb.core.MongoOperations;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.Query;
+
 
 import java.util.Date;
+import java.util.List;
 
 @SpringBootApplication
 public class Application implements CommandLineRunner {
@@ -36,6 +44,8 @@ public class Application implements CommandLineRunner {
 
     @Override
     public void run(String... args) throws Exception {
+
+
 
         customerRepository.deleteAll();
 
@@ -73,13 +83,69 @@ public class Application implements CommandLineRunner {
 
         todoRepository.deleteAll();
 
-        todoRepository.save(new Todo("hola", 5,new Date(),"j@mail.com","pending"));
-        todoRepository.save(new Todo("hola2", 5,new Date(),"j@mail.com","pending"));
-        todoRepository.save(new Todo("hola3", 5,new Date(),"d@mail.com","pending"));
+        todoRepository.save(new Todo("task1", 5,new Date(2020,10,23),"j@mail.com","pending"));
+        todoRepository.save(new Todo("task2", 5,new Date(2019,10,02),"j@mail.com","pending"));
+        todoRepository.save(new Todo("task3", 5,new Date(2020,10,10),"a@mail.com","pending"));
+        todoRepository.save(new Todo("task4", 5,new Date(2022,11,21),"j@mail.com","pending"));
+        todoRepository.save(new Todo("task5", 5,new Date(2021,12,14),"b@mail.com","pending"));
+        todoRepository.save(new Todo("task6", 5,new Date(2020,12,13),"c@mail.com","pending"));
+        todoRepository.save(new Todo("task7", 5,new Date(2020,9,12),"d@mail.com","pending"));
+        todoRepository.save(new Todo("task8", 5,new Date(2020,12,3),"e@mail.com","pending"));
+        todoRepository.save(new Todo("task9", 5,new Date(2021,11,12),"f@mail.com","pending"));
+        todoRepository.save(new Todo("task10", 5,new Date(2020,12,17),"g@mail.com","pending"));
+        todoRepository.save(new Todo("task11", 5,new Date(2020,12,18),"j@mail.com","pending"));
+        todoRepository.save(new Todo("task12", 5,new Date(2020,12,1),"h@mail.com","pending"));
+        todoRepository.save(new Todo("task13", 5,new Date(2020,12,19),"j@mail.com","pending"));
+        todoRepository.save(new Todo("task14", 5,new Date(2020,12,21),"j@mail.com","pending"));
+        todoRepository.save(new Todo("task15", 5,new Date(2020,12,11),"i@mail.com","pending"));
+        todoRepository.save(new Todo("task16", 5,new Date(2020,6,17),"a@mail.com","pending"));
+        todoRepository.save(new Todo("task17", 5,new Date(2020,5,17),"a@mail.com","pending"));
+        todoRepository.save(new Todo("task19", 5,new Date(2020,4,17),"b@mail.com","pending"));
+        todoRepository.save(new Todo("task20", 5,new Date(2020,3,17),"d@mail.com","pending"));
+        todoRepository.save(new Todo("task21", 5,new Date(2023,12,17),"i@mail.com","pending"));
+        todoRepository.save(new Todo("task22", 5,new Date(2024,12,17),"d@mail.com","pending"));
+        todoRepository.save(new Todo("task23", 5,new Date(2025,12,17),"j@mail.com","pending"));
+        todoRepository.save(new Todo("task24", 5,new Date(2026,12,17),"j@mail.com","pending"));
+        todoRepository.save(new Todo("task25", 5,new Date(2027,12,17),"d@mail.com","pending"));
+
+
+
          todoRepository.findByResponsibleContaining("j@mail.com", PageRequest.of(0, 2)).stream()
                 .forEach(System.out::println);
 
         System.out.println();
+
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfiguration.class);
+        MongoOperations mongoOperation = (MongoOperations) applicationContext.getBean("mongoTemplate");
+
+        Query query = new Query();
+        query.addCriteria(Criteria.where("firstName").is("Alice"));
+        Customer customer = mongoOperation.findOne(query, Customer.class);
+        System.out.println(customer);
+
+        //dueDate has expired
+
+        Query query2 = new Query();
+        query.addCriteria(Criteria.where("dueDute").lt(new Date()));
+        List<Todo> todos=mongoOperation.find(query2,Todo.class);
+        System.out.println(todos);
+
+        //Todos that are assigned to given user and have priority greater equal to 5
+        Query query3 = new Query();
+        query.addCriteria(Criteria.where("responsible").is("j@mail.com").and("priority").gt(4));
+        List<Todo> todos2=mongoOperation.find(query3,Todo.class);
+        System.out.println(todos2);
+
+        //Todos that contains a description with a length greater than 30 characters
+        Query query4 = new Query();
+        query.addCriteria(Criteria.where("description")..gt(4));
+        List<Todo> todos4=mongoOperation.find(query3,Todo.class);
+        System.out.println(todos4);
+
+
+
+
+
 
 
 
